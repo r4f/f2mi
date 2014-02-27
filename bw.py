6a64a5861d36383b9f1b1b90d4bc916cc154b8b9
Rafael Arndt <rafaelarndt@gmail.com>
2014-02-27 10:30:01 +0100

    On branch master  Changes to be committed: 	new file:   bw.py 	new file:   gamelayout.py

---
+++ bw.py
@@ -0,0 +1,107 @@
+import random
+import sys
+import math
+
+n=2
+m=n*n
+buttons_state=[0]*m
+wires=[[0]*m for j in range(m)]
+task=[0]*m
+
+def new_task():
+	global wires
+	global task
+	wires=[[random.randint(0,1) for i in range(m)] for j in range(m)]
+#	for i in range(m):
+#		print(wires[i])
+	solution=[random.randint(0,1) for i in range(m)]
+#	print(solution)
+#	draw(task)
+	for button in range(m):
+		if solution[button]:
+			for i in range(m):
+				task[i]=wires[button][i]!=task[i]
+	print("New task:")
+	draw(task)
+	print("Buttons:")
+	draw_buttons()
+	print("you:")
+	draw(buttons_state)
+
+def draw(vector):
+	out='+'+'-'*n+'+\n'
+	for i in range(n):
+		out=out+'|'
+		for j in range(n):
+			if vector[i*n+j]:
+				out=out+"x"
+			else:
+				out=out+" "
+		out=out+'|\n'
+	out=out+'+'+'-'*n+'+'
+	print(out)
+
+def draw_buttons():
+	o=" "
+	p=''
+	q=''
+	r=''
+	for i in range(m):
+		o=o+" "*int(math.trunc(0.5*n-0.5))+str(i+1)+" "*int(math.ceil(0.5*n+0.5))
+		p=p+'+'+'-'*n
+		q=q+'+'+'-'*n
+	for i in range(n):
+		for j in range(m):
+			r=r+'|'
+			for k in range(n):
+				if wires[j][i*n+k]:
+					r=r+'*'
+				else:
+					r=r+" "
+		r=r+'|\n'
+	o=o+'\n'
+	p=p+'+\n'
+	q=q+'+'
+	print(o+p+r+q)
+
+def state():
+	draw(buttons_state)
+
+def show_task():
+	print("task:")
+	draw(task)
+	print("you:")
+	draw(buttons_state)
+
+def press(button):
+	for i in range(m):
+		buttons_state[i]=wires[button][i]!=buttons_state[i]
+	state()
+
+new_task()
+
+goon=True
+parsed=False
+while goon:
+	inp = raw_input("Press button: ")
+	for i in range(1,m+1):
+		if str(i)==inp:
+			press(i-1)
+			if buttons_state==task:
+				print("Congratulations!")
+				new_task()
+			parsed=True
+	if parsed == False:
+		if str(0) == inp:
+			show_task()
+		elif inp == 'n':
+			new_task()
+		elif inp == 'b':
+			draw_buttons()
+		elif inp == 'h':
+			print("for buttons action, press 1 to "+str(m)+"\nor press 0 to show task,\nor b to see how the buttons behave\nor n for new round\nor x if you don't want to play anymore!")
+		elif inp == 'x':
+			goon=False
+		else:
+			print("did not understand")
+	parsed=False
---
+++ gamelayout.py
@@ -0,0 +1,81 @@
+from tkinter import *
+from tkinter import ttk
+from math import *
+import random
+
+def newgame():
+	print("new game")
+def enter(args):
+	print("enter")
+def change_state():
+	state = [random.randint(0,1) for _ in range(4)]
+	for i in range(len(state)):
+		x=floor(i/2)
+		y=i%2
+		if state[i]==1:
+			color="black"
+		else:
+			color="white"
+		cv.create_rectangle((x*200, y*200, x*200+200, y*200+200), fill=color)
+	tries.set(int(tries.get())+1)
+def callback(event):
+	offset=floor(event.x/100)
+	if buttons_state[offset]:
+		buttons_state[offset]=0
+		color="blue"
+	else:
+		buttons_state[offset]=1
+		color="green"
+	row.create_rectangle((offset*100, 100, 100+offset*100, 120), fill=color)
+
+buttons_state=[0,0,0,0]
+
+root = Tk()
+root.title("F2Mi")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+tries = StringVar()
+tries.set(00)
+
+tries_label = ttk.Label(mainframe, textvariable=tries)
+tries_label.grid(column=0, row=0, columnspan=2, sticky=(W, E))
+
+newgame_button=ttk.Button(mainframe, text="New Game", command=newgame)
+newgame_button.grid(column=2, row=0, columnspan=2, sticky=(W, E))
+cv=Canvas(mainframe, background="blue",  width=400, height=400)
+cv.grid(column=0, row=1, columnspan=4, sticky=(W, E, S, N))
+
+row=Canvas(mainframe, background="blue",  width=400, height=120)
+row.grid(column=0, row=2, columnspan=4, sticky=(W, E, S, N))
+
+states = [[0,1,1,0],[1,1,1,0],[0,0,1,0],[0,1,0,1]]
+for i in range(len(states)):
+	offset=i*100
+	for j in range(len(states[i])):
+		x=j%2
+		y=floor(j/2)
+		if states[i][j]==1:
+			color="black"
+		else:
+			color="white"
+		row.create_rectangle((x*50+offset, y*50, x*50+50+offset, y*50+50), fill=color)
+
+row.bind("<Button-1>", callback)
+
+#ttk.Label(mainframe, text="1").grid(column=0, row=2, sticky=E)
+#ttk.Label(mainframe, text="2").grid(column=1, row=2, sticky=E)
+#ttk.Label(mainframe, text="3").grid(column=2, row=2, sticky=E)
+#ttk.Label(mainframe, text="4").grid(column=3, row=2, sticky=E)
+
+ok_button=ttk.Button(mainframe, text="ok", command=change_state)
+ok_button.grid(column=0, row=3, columnspan=4, sticky=(W, E))
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+newgame_button.focus()
+root.bind('<Return>', enter)
+
+root.mainloop()
